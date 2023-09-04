96f044fe3c2b722c00f059de6e545293a3bab54a
muraliweb9 <muraliweb@hotmail.com>
2023-09-04 13:02:27 +0100

    Initial

---
+++ src/main/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/EobaoObboardingAppApplication.java
@@ -0,0 +1,13 @@
+package com.hackearth.goldenticket.mmg.eobaoobboardingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EobaoObboardingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EobaoObboardingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/EobaoObboardingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackearth.goldenticket.mmg.eobaoobboardingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EobaoObboardingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

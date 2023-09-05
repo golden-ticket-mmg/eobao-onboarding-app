6af6994948163a25005c7fea01da7983398b8c51
muraliweb9 <muraliweb@hotmail.com>
2023-09-05 09:31:22 +0100

    Initial

--- src/main/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/EobaoObboardingAppApplication.java
+++ src/main/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/EobaoObboardingAppApplication.java
@@ -1,14 +1,17 @@
 package com.hackearth.goldenticket.mmg.eobaoobboardingapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class EobaoObboardingAppApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting EobaoObboardingAppApplication ....");
 		SpringApplication.run(EobaoObboardingAppApplication.class, args);
 	}
 
--- src/main/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/service/RestEntryPoint.java
+++ src/main/java/com/hackearth/goldenticket/mmg/eobaoobboardingapp/service/RestEntryPoint.java
@@ -1,6 +1,7 @@
 package com.hackearth.goldenticket.mmg.eobaoobboardingapp.service;
 
 import com.hackearth.goldenticket.mmg.eobaoobboardingapp.data.Company;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("onboarding")
+@Slf4j
 public class RestEntryPoint {
 
     private RestTemplate restTemplate;
@@ -20,11 +22,13 @@ public class RestEntryPoint {
 
     @GetMapping("/{arn}")
     public Company getCompany(@PathVariable String arn) {
+        log.info("Looking for company by ARN {}", arn);
         ResponseEntity<Company> responseEntity = restTemplate
                 .getForEntity("http://localhost:9502/online-data/" + arn,
                         Company.class);
 
         Company company = responseEntity.getBody();
+        log.info("For ARN {} found company {}", arn, company);
 
         return company;
 
